f004a54b8ca1f1fc27d0968d59bfb3af3f852435
Carlos Palacios <carlosadrian.palaciosgonzalez@nxp.com>
2018-12-02 13:46:51 -0600

    changes in InicioController

--- ltv-application-1.1/src/Application/InicioController.java
+++ ltv-application-1.1/src/Application/InicioController.java
@@ -1,39 +1,66 @@
 package Application;
 
+import javax.swing.JOptionPane;
+
+import Database.DataBase;
 import Database.UsuariosTable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class InicioController extends UsuariosTable {
+public class InicioController {
 
 	///////////////////////////////////////////////////////////////////////////
 	// Attributes /////////////////////////////////////////////////////////////
 	public Button			btnInicio;
 	public TextField		txtUsuario;
 	public PasswordField	txtContrasena;
-	public Main objMain = new Main();
+	
+	Main objMain = new Main();
+	DataBase objDatabase = new DataBase();
+	UsuariosTable user = new UsuariosTable();
 
 	///////////////////////////////////////////////////////////////////////////
 	// Methods ////////////////////////////////////////////////////////////////
-	public void vdInicio () {
-		findUSUARIO(txtUsuario.getText());
-		if(getCLAVE().equals(txtContrasena.getText())) {
-			btnInicio.getScene().getWindow().hide();
-			try {
-				switch (getCARGO()) {
-				case 'R': objMain.changeScene("ReservacionesScene.fxml");	break;
-				case 'A': objMain.changeScene("AdministracionScene.fxml");	break;
-				case 'C': objMain.changeScene("ContabilidadScene.fxml");	break;
-				case 'D': objMain.changeScene("DireccionScene.fxml");		break;
+	public void callInicio () {
+		try {
+			objDatabase.PrepareUsuarios();
+			user = objDatabase.CheckUsuarios(txtUsuario.getText());
+			if(user.getCLAVE().equals(txtContrasena.getText())) {
+				btnInicio.getScene().getWindow().hide();
+				switch (user.getCARGO().charAt(0)) {
+					case 'R': objMain.changeScene("ReservacionesScene.fxml");	break;
+					case 'A': objMain.changeScene("AdministracionScene.fxml");	break;
+					case 'C': objMain.changeScene("ContabilidadScene.fxml");	break;
+					case 'D': objMain.changeScene("DireccionScene.fxml");		break;
 				default: break;
 				}
-			}catch (Exception e) {
-				errMess(" Error al iniciar sesión.");
+			}else {
+				System.out.println("Contraseña incorrecta");
 			}
-		}else {
-			errMess(" Contraseña incorrecta.");
+			
+		}catch(Exception e) {
+			
 		}
+		
+		
+//		findUSUARIO(txtUsuario.getText());
+//		if(getCLAVE().equals(txtContrasena.getText())) {
+//			btnInicio.getScene().getWindow().hide();
+//			try {
+//				switch (getCARGO()) {
+//				case 'R': objMain.changeScene("ReservacionesScene.fxml");	break;
+//				case 'A': objMain.changeScene("AdministracionScene.fxml");	break;
+//				case 'C': objMain.changeScene("ContabilidadScene.fxml");	break;
+//				case 'D': objMain.changeScene("DireccionScene.fxml");		break;
+//				default: break;
+//				}
+//			}catch (Exception e) {
+//				errMess(" Error al iniciar sesión.");
+//			}
+//		}else {
+//			errMess(" Contraseña incorrecta.");
+//		}
 	}
 
 }
